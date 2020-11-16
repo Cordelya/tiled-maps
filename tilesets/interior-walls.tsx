e25f43dce5c01e3279018761b438467280cd6e74
Jacqueline Arsenault <justcordelya@gmail.com>
2020-11-16 14:39:21 -0500

    Signs adjustment
    Adjusted signs to be blank until we attach a video. Sign with papers exported as an object for attaching/placing image/video objects

---
+++ tilesets/interior-walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.10.30" name="interior-walls" tilewidth="32" tileheight="32" tilecount="444" columns="12">
+ <image source="../tilesets/interior-walls-bleutailfly-combined.png" width="400" height="1200"/>
+</tileset>
---
+++ tilesets/medieval-buildings/medieval-buildings-corrected-door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.10.30" name="medieval-buildings-corrected-door" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../../../Downloads/medieval-buildings-corrected-door.png" width="512" height="512"/>
+</tileset>
